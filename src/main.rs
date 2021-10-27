69bea6c9ec39f0f1e1fb5d67a12aa9ed9acae0e7
FeistyKit <eeveebeevee33@gmail.com>
2021-10-26 22:05:04 -0400

    Got some very simple tui stuff running

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
 mod storage;
 mod vocab;
 
-fn main() {}
+use crate::storage::AnyResult;
+
+use tui::widgets;
+
+fn prepare_terminal() -> AnyResult<tui::Terminal<tui::backend::CrosstermBackend<std::io::Stdout>>> {
+    // https://docs.rs/tui/0.16.0/tui/index.html
+    let stdout = std::io::stdout();
+    let backend = tui::backend::CrosstermBackend::new(stdout);
+    Ok(tui::Terminal::new(backend)?)
+}
+
+fn main() -> AnyResult<()> {
+    let mut term = prepare_terminal()?;
+    term.draw(termdraw)?;
+    Ok(())
+}
+
+fn termdraw<'a>(f: &mut tui::terminal::Frame<'_, tui::backend::CrosstermBackend<std::io::Stdout>>) {
+    let size = f.size();
+    let block = widgets::Block::default()
+        .title("Block")
+        .borders(widgets::Borders::ALL);
+    f.render_widget(block, size);
+}
